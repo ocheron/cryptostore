6c8d851ec0dbd44a038175ba583fef94f33d6b37
Olivier Chéron <olivier.cheron@gmail.com>
2019-03-20 06:43:21 +0100

    Replace getter/setter pair with lens

--- src/Crypto/Store/CMS/Info.hs
+++ src/Crypto/Store/CMS/Info.hs
@@ -25,6 +25,7 @@ import Control.Monad.Fail (MonadFail)
 
 import Data.ASN1.Types
 import Data.ByteString (ByteString)
+import Data.Functor.Identity
 import Data.Maybe (isJust, isNothing)
 
 import Crypto.Store.ASN1.Generate
@@ -124,38 +125,37 @@ parseData = do
 -- Functions 'toAttachedCI' and 'toDetachedCI' are needed to decide about the
 -- outer encapsulation state and build a 'ContentInfo'.
 class Encapsulates struct where
-    getInner :: struct a -> a
-    setInner :: struct a -> b -> struct b
+    lens :: Functor f => (a -> f b) -> struct a -> f (struct b)
     toCI :: struct (Encap ByteString) -> ContentInfo
 
 instance Encapsulates SignedData where
-    getInner = sdEncapsulatedContent
-    setInner s c = s { sdEncapsulatedContent = c }
+    lens f s = let g a = s { sdEncapsulatedContent = a }
+                in fmap g (f $ sdEncapsulatedContent s)
     toCI = SignedDataCI
 
 instance Encapsulates EnvelopedData where
-    getInner = evEncryptedContent
-    setInner s c = s { evEncryptedContent = c }
+    lens f s = let g a = s { evEncryptedContent = a }
+                in fmap g (f $ evEncryptedContent s)
     toCI = EnvelopedDataCI
 
 instance Encapsulates DigestedData where
-    getInner = ddEncapsulatedContent
-    setInner s c = s { ddEncapsulatedContent = c }
+    lens f s = let g a = s { ddEncapsulatedContent = a }
+                in fmap g (f $ ddEncapsulatedContent s)
     toCI = DigestedDataCI
 
 instance Encapsulates EncryptedData where
-    getInner = edEncryptedContent
-    setInner s c = s { edEncryptedContent = c }
+    lens f s = let g a = s { edEncryptedContent = a }
+                in fmap g (f $ edEncryptedContent s)
     toCI = EncryptedDataCI
 
 instance Encapsulates AuthenticatedData where
-    getInner = adEncapsulatedContent
-    setInner s c = s { adEncapsulatedContent = c }
+    lens f s = let g a = s { adEncapsulatedContent = a }
+                in fmap g (f $ adEncapsulatedContent s)
     toCI = AuthenticatedDataCI
 
 instance Encapsulates AuthEnvelopedData where
-    getInner = aeEncryptedContent
-    setInner s c = s { aeEncryptedContent = c }
+    lens f s = let g a = s { aeEncryptedContent = a }
+                in fmap g (f $ aeEncryptedContent s)
     toCI = AuthEnvelopedDataCI
 
 -- | Return 'True' when the encapsulated content is attached.
@@ -165,12 +165,12 @@ isAttached = isJust . fromAttached
 -- | Unwrap the encapsulation, assuming the inner content is inside the data
 -- structure.  The monadic computation fails if the content was detached.
 fromAttached :: (MonadFail m, Encapsulates struct) => struct (Encap a) -> m (struct a)
-fromAttached s = fromEncap err (return . setInner s) (getInner s)
+fromAttached = lens (fromEncap err return)
   where err = fail "fromAttached: detached"
 
 -- | Keep the content inside the data structure.
 toAttached :: Encapsulates struct => struct a -> struct (Encap a)
-toAttached s = setInner s (Attached $ getInner s)
+toAttached = runIdentity . lens (Identity . Attached)
 
 -- | Transform the data structure into a content info, keeping the encapsulated
 -- content attached.  May be applied to structures with 'EncapsulatedContent' or
@@ -185,12 +185,12 @@ isDetached = isNothing . fromAttached
 -- | Recover the original data structure from a detached encapsulation and the
 -- external content.  The monadic computation fails if the content was attached.
 fromDetached :: (MonadFail m, Encapsulates struct) => b -> struct (Encap a) -> m (struct b)
-fromDetached c s = fromEncap (return $ setInner s c) err (getInner s)
+fromDetached c = lens (fromEncap (return c) err)
   where err _ = fail "fromDetached: attached"
 
 -- | Remove the content from the data structure to store it externally.
 toDetached :: Encapsulates struct => struct a -> (a, struct (Encap a))
-toDetached s = (getInner s, setInner s Detached)
+toDetached = let f a = (a, Detached) in lens f
 
 -- | Transform the data structure into a content info, detaching the
 -- encapsulated content.  May be applied to structures with
